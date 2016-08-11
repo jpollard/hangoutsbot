f5b346f9d9acce7090f76b0e671334d5486f44a5
Joseph Pollard <joseph.w.pollard+git@gmail.com>
2016-08-11 01:22:10 -0400

    A simple plugin that says something anytime the hangout name is changed.

---
+++ hangupsbot/plugins/rename.py
@@ -0,0 +1,29 @@
+"""
+   A hangouts bot plugin that makes a comment anytime the hangouts name is changed.
+"""
+import plugins
+import random
+import asyncio
+
+def _initialise(bot):
+    plugins.register_handler(_watch_rename, type='rename')
+    return []
+
+@asyncio.coroutine
+def _watch_rename(bot, event, command):
+    # Dont handle events caused by bot
+    if event.user.is_self:
+        return
+
+    # Else say something clever
+    else:
+        return bot.coro_send_message(event.conv, _("{}").format(get_response()))
+
+def get_response():
+    responses = ["That's a shitty new name",
+                 "I like that name",
+                 "Change it back!",
+                 "Hahaha, that's funny.",
+
+                 ]
+    return responses[random.randint(0, len(responses) - 1)]
